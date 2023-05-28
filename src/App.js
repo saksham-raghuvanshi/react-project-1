f3fbd1623ce1b8b858a0cf7efb36cda2718beed4
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-05-28 14:32:32 +0530

    Responsive added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
-// import Footer from "./Components/Footer";
+
 import "../src/styles/App.scss"
 import "../src/styles/Header.scss"
 import "../src/styles/Home.scss"
-import "../src/styles/Footer.scss"
+
 
 
 function App() {
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,17 +7,19 @@ class Header extends Component {
     this.setState({clicked: !this.state.clicked})
 
   }
+
+
   render(){
   return (
     <nav>
         <Link to={"/"} ><h1>Frontend Gyaan</h1> </Link>
         <main>
         <ul id="navb"  className={this.state.clicked ? "#navb active" : "#navb"}>
-            <Link className="active" to={"/"}>Home</Link>
-            <Link to={"/contact-us"}>Contact</Link>
-            <Link to={"/about-us"}>About</Link>
-            <Link to={"/brands"}>Brands</Link>
-            <Link to={"/services"}>Services</Link>
+            <Link onClick={this.handleClick} to={"/"}>Home</Link>
+            <Link onClick={this.handleClick} to={"/contact-us"}>Contact</Link>
+            <Link onClick={this.handleClick} to={"/about-us"}>About</Link>
+            <Link onClick={this.handleClick} to={"/blogs"}>Blogs</Link>
+            <Link onClick={this.handleClick} to={"/learn"}>Learn</Link>
           </ul>
         </main>
         <div className="mobile" onClick={this.handleClick}>
